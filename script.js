a01ad8ce0c675a0c2ad362d6d931033912c15867
Saylendra <saylendrayasin@gmail.com>
2021-06-28 20:10:46 +0800

    Save

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const menu = document.querySelector('.menu input');
 const nav = document.querySelector('nav ul');
 
 menu.addEventListener('click',function(){
-    nav.classList.toggle('slide')
+    nav.classList.toggle('Aktif')
 });
+
